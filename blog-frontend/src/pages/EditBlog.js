24e2e678d2114f05236241d8a04095d11d5b7b0c
NEELIMA2004GUPTA <niyati123gupta@gmail.com>
2025-10-17 17:31:12 +0530

    s

--- blog-frontend/src/pages/EditBlog.js
+++ blog-frontend/src/pages/EditBlog.js
@@ -21,8 +21,7 @@ export default function EditBlog() {
   // Prefill blog
   useEffect(() => {
     if (id && token) {
-      axios
-        .get(`http://127.0.0.1:8000/api/blogs/${id}/`, {
+      axios.get(`http://127.0.0.1:8000/api/blogs/${id}/`, {
           headers: { Authorization: `Bearer ${token}` },
         })
         .then((res) => {

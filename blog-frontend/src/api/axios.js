49a3533a49175da4df5477dc5dc88802521dd475
NEELIMA2004GUPTA <niyati123gupta@gmail.com>
2025-10-24 15:51:59 +0530

    s

--- blog-frontend/src/api/axios.js
+++ blog-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://omar-heady-paulina.ngrok-free.dev/api',
+  baseURL: 'https://omar-heady-paulina.ngrok-free.dev/api',           
 });
 
 // Add token automatically if stored
